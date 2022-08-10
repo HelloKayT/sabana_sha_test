8b51359d16afe22fa4d144f53096b1a974c4a924
Luis Vega <vegaluisjose@users.noreply.github.com>
2022-08-09 22:03:39 -0700

    add fixes (#1)

--- tests/test_sha_test.py
+++ tests/test_sha_test.py
@@ -16,16 +16,38 @@ from pathlib import Path
 import numpy as np
 from sabana import Instance, Program
 
+def create_program(file):
+    in_len = 16
+    out_len = 8
+    dtype = np.int32
+    start = np.ones([1], dtype)
+    finish = np.array([14], dtype)
+    a = np.fromfile(file, dtype=np.uint8)
+    a_len = np.array([in_len], dtype=dtype)
+    y_len = np.array([out_len], dtype=dtype)
+    program = Program()
+    program.mmio_alloc(name="c0", size=0x00010000, base_address=0xA0000000)
+    program.buffer_alloc(name="a", size=a.nbytes, mmio_name="c0", mmio_offset=0x10)
+    program.buffer_alloc(name="y", size=out_len*4, mmio_name="c0", mmio_offset=0x24)
+    program.mmio_write(a_len, name="c0", offset=0x1C)
+    program.mmio_write(y_len, name="c0", offset=0x30)
+    program.mmio_write(start, name="c0", offset=0x0)
+    program.mmio_wait(finish, name="c0", offset=0x0, timeout=3)
+    program.buffer_read(name="y", offset=0, dtype=np.uint8, shape=(out_len*4,))
+    program.mmio_dealloc(name="c0")
+    program.buffer_dealloc(name="a")
+    program.buffer_dealloc(name="y")
+    return program
 
 def test_main():
-
-    # deploy instance
+    file = Path(__file__).resolve().parent.joinpath("single_block.txt.padded")
+    prog = create_program(file)
     image_file = Path(__file__).resolve().parent.parent.joinpath("sabana.json")
     inst = Instance(image_file=image_file, verbose=True)
     inst.up()
-    # terminate instance
+    responses = inst.execute(prog)
     inst.down()
-
+    print(responses[0])
 
 
 if __name__ == "__main__":
